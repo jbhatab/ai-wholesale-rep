463cbe5278c6f1ebba96f2df71ee76642eacfd1c
Blaine Hatab <jbhatb@gmail.com>
2024-12-30 17:14:42 -0500

    moved assistant prompt and cleaned up bad text a little

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import ActiveCallDetail from "./components/ActiveCallDetail";
 import Button from "./components/base/Button";
 import Vapi from "@vapi-ai/web";
 
+import { bob } from "./assistants";
+
 const vapi = new Vapi(process.env.REACT_APP_VAPI_PUBLIC_KEY || process.env.PUBLIC_VAPI_KEY || process.env.NEXT_PUBLIC_VAPI_KEY  || "");
 
 const App = () => {
@@ -50,7 +52,7 @@ const App = () => {
   // call start handler
   const startCallInline = () => {
     setConnecting(true);
-    vapi.start(assistantOptions);
+    vapi.start(assistant);
   };
   const endCall = () => {
     vapi.stop();
@@ -119,89 +121,6 @@ const App = () => {
       </div>
     </div>
   );
-};
-
-const assistantOptions = {
-  name: "Bob",
-  firstMessage: "Hello! Can I take your order?",
-  transcriber: {
-    provider: "deepgram",
-    model: "nova-2",
-    language: "en-US",
-  },
-  voice: {
-    provider: "cartesia",
-    voiceId: "565510e8-6b45-45de-8758-13588fbaec73",
-  },
-  analysisPlan: {
-    structuredDataPlan: {
-      enabled: true,
-      schema: {
-        type: "array",
-        items: {
-          type: "object",
-          properties: {
-            productName: { type: "string", description: "name or sku of the product being ordered"},
-            quantity: { type: "number", description: "The quantity of the product being ordered"},
-          }
-        },
-        description: "string"
-      },
-      timeoutSeconds: 1
-    }
-  },
-  model: {
-    provider: "openai",
-    model: "gpt-4",
-    messages: [
-      {
-        role: "system",
-        content: `You are Bob, a voice assistant for a wholsale Cannabis company that sells directly to dispensaries.
-
-Your job is to take the order of wholesale customers calling in. 
-
-The menu consists of the following with inventory levels next to the product name in this fashion "{Product Name} - {Inventory Count} {Inventory Unit Type}":
-
-1) Crude Oil - 1908.8 grams
-2) Green Crack Trim - 1360 lbs
-3) Platinum OG Trim - 171 lbs
-4) Prerolled Joint - 597 units
-5) RAW Cone - 112 units
-
-
-1) There are 3 kinds of Eighths: Green Crack, Blue Dream, and OG Kush
-2) There are 2 kinds of Vapes: Jack Herer and Blueberry Yum Yum
-3) There are 2 kinds of Edibles: Chocolate and Gummies
-
-Customers can only place an order for a sku if there is inventory available. If a customer tries to order more quanitity of a product
-than is available, politely inform them how much is available and have them update the order quantity. If the amount ordered is available, then accept that item in the order!
-
-Customers must order at least one item.
-
-Don't talk much or at all while they are placing the order. Give them some time to finish the order and only interrupt if we dont have enough inventory or a sku in stock.
-
-Assume the customer has visual access to the menu and inventory levels. If they place an order and the item doesn't exist, try to suggest an item that best matches what you think they are suggesting.
-
-After the order is placed, repeat the order back to the customer to confirm it.
-
-If the customer goes off-topic or off-track and talks about anything but the
-process of ordering, politely steer the conversation back to collecting their order.
-
-Once you have all the information you need pertaining to their order, you can
-end the conversation. You can say something like "Awesome, we'll have that ready
-for you in 10-20 minutes." to naturally let the customer know the order has been
-fully communicated.
-
-It is important that you collect the order in an efficient manner (succinct replies
-& direct questions). You only have 1 task here, and it is to collect the customers
-order, then end the conversation.
-
-- Be sure to be kind of funny and witty!
-- Keep all your responses short and simple. Use casual language, phrases like "Umm...", "Well...", and "I mean" are preferred.
-- This is a voice conversation, so keep your responses short, like in a real conversation. Don't ramble for too long.`,
-      },
-    ],
-  },
 };
 
 export default App;
